1d2de0c25df59ab4a32fe71d49d341364ece31f6
hamza sellah <hamzasellah101294@gmail.com>
2023-06-08 15:54:56 +0100

    add backendApp

---
+++ frontendApp/src/app/components/pages/cart-page/cart-page.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { Cart } from 'src/app/shared/models/Cart';
+import { CartItem } from 'src/app/shared/models/CartItem';
+
+@Component({
+  selector: 'app-cart-page',
+  templateUrl: './cart-page.component.html',
+  styleUrls: ['./cart-page.component.css']
+})
+export class CartPageComponent {
+  cart!:Cart;
+
+  constructor(private _cartService: CartService){
+    this._cartService.getCartObservable().subscribe(cart => {
+      this.cart = cart;});
+  }
+
+  removeFromCart(cartItem:CartItem){
+    this._cartService.removeFromCart(cartItem.food.id);
+  }
+
+  changeQuantity(cartItem:CartItem,quantityInString:string){
+    const quantity = parseInt(quantityInString);
+    this._cartService.changeQuantity(cartItem.food.id,quantity);
+
+  }
+
+
+
+
+
+}
---
+++ frontendApp/src/app/components/pages/food-page/food-page.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/models/Food';
+
+@Component({
+  selector: 'app-food-page',
+  templateUrl: './food-page.component.html',
+  styleUrls: ['./food-page.component.css']
+})
+export class FoodPageComponent {
+  food!:Food;
+
+  constructor(activatedRoute:ActivatedRoute,private _foodService:FoodService,
+   private _cartService:CartService,private router:Router){
+    activatedRoute.params.subscribe((params)=>{
+    if(params.id)
+    this.food = _foodService.getFoodById(params.id);
+    })
+  }
+
+
+  addToCart(){
+    this._cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
+
+  }
+
+
+
+
+}

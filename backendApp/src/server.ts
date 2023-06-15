04336c9095d1b65b1c5be7fbaa967a5b838943fc
hamza sellah <hamzasellah101294@gmail.com>
2023-06-15 13:12:39 +0100

    new update

--- backendApp/src/server.ts
+++ backendApp/src/server.ts
@@ -1,8 +1,10 @@
 import express from "express" ;
 import cors from "cors";
-import { sample_foods, sample_tags } from "./data";
+import { sample_foods, sample_tags, sample_users } from "./data";
+import jwt from "jsonwebtoken" ;
 
 const app = express();
+app.use(express.json());
 app.use(cors({
     credentials:true,
     origin:["http://localhost:4200"]
@@ -41,13 +43,28 @@ app.get("/api/foods/:foodId",(req,res)=>{
 
 })
 
+app.post("/api/users/login",(req,res)=>{
+    const {email,password}=req.body;
+    const user = sample_users.find(user => 
+      user.email === email && user.password === password);
 
+    if(user)
+       res.send(generateTokenResponse(user));
+    else 
+       res.status(400).send("User name or password is not valid!!");
 
+})
 
+const generateTokenResponse = (user:any)=>{
+    const token = jwt.sign({
+      email:user.email,isAdmin:user.isAdmin
+    },"SomeRandomText",{expiresIn:"30d"});
 
+    user.token = token;
+    return user;
 
+}
 
-app.get("/api/foods/")
 const port = 5000;
 app.listen(port,() => {
     console.log("website served on http://localhost:"+ port);
